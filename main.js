5c4b738fe422c115dc1ded7badf35800c8717bd9
Levon Chilingaryan <chilingaryan1990@list.ru>
2023-02-22 20:37:40 +0400

    date project

---
+++ main.js
@@ -0,0 +1,35 @@
+const el_1 = document.getElementById("date_1");
+const date_1 = new Date()
+
+el_1.innerHTML = date_1.toLocaleDateString();
+
+const el_2 =document.getElementById("date_2")
+const date_2 = new Date()
+
+el_2.innerHTML = date_2.toLocaleDateString();
+
+const el_3 =document.getElementById("date_3")
+const date_3 = new Date()
+
+el_3.innerHTML = date_3.toLocaleDateString();
+
+
+const el_4 =document.getElementById("date_4")
+const date_4 = new Date()
+
+el_4.innerHTML = date_4.toLocaleDateString();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
