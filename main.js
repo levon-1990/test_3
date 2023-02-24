c9b90d56df85ff3896d3bc9901036ce4c0a68c88
Levon Chilingaryan <chilingaryan1990@list.ru>
2023-02-24 14:29:08 +0400

    change project

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
-const el_1 = document.getElementById("date_1");
-const date_1 = new Date()
+let date = ["date_1", "date_2", "date_3","date_4"];
+let el= 0;
+let date_index = new Date()
 
-el_1.innerHTML = date_1.toLocaleDateString();
-
-const el_2 =document.getElementById("date_2")
-const date_2 = new Date()
-
-el_2.innerHTML = date_2.toLocaleDateString();
-
-const el_3 =document.getElementById("date_3")
-const date_3 = new Date()
-
-el_3.innerHTML = date_3.toLocaleDateString();
+for (let i = 0; i < date.length; i++) {
+  let date_span = date[i]
+   el = document.getElementById(date_span);
+   el.innerHTML = date_index.toLocaleDateString();
+}
 
 
-const el_4 =document.getElementById("date_4")
-const date_4 = new Date()
 
-el_4.innerHTML = date_4.toLocaleDateString();
 
 
 
